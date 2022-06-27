229822a23974dfbbdeaaf0734f5b770944bc27f5
adnanali <alkholaidi@verizon.net>
2022-06-27 10:00:53 -0400

    admin-panel-added

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       <StoreProvider>
         <PayPalScriptProvider deferLoading={true}>
           {Component.auth ? (
-            <Auth>
+            <Auth adminOnly={Component.auth.adminOnly}> 
               <Component {...pageProps} />
             </Auth>
           ) : (
@@ -20,11 +20,11 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       </StoreProvider>
     </SessionProvider>
   ); 
-}       
+}        
 
-function Auth({children}) {
+function Auth({children, adminOnly}) {
   const router = useRouter();
-  const {status} = useSession({
+  const {status, data: session} = useSession({
     required: true,
     onUnauthenticated() {
       router.push('/unauthorized?message=login required');
@@ -33,7 +33,11 @@ function Auth({children}) {
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
+  if (adminOnly && !session.user.isAdmin) {
+    router.push('/unauthorized?message=admin login required');
+  }
+
   return children;
 } 
 
-export default MyApp;                           
+export default MyApp;                             
